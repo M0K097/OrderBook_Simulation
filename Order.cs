1f779bf6b5f3f17d2ce88d7492788761aeb49db4
M0K0 <m0k097@gmx.de>
2026-05-06 13:42:30 +0200

    made static order id generation

--- Order.cs
+++ Order.cs
@@ -15,18 +15,33 @@ public enum Status
 
 public abstract class Order 
 {
+    public static int order_id_counter = 0;
+
     public int order_id {get;}
     public Side order_side {get;}
     public double quantity {get;}
     public decimal price {get;}
     public DateTime time {get;}
-    public Status status {get;set;}
-    public double filled {get;set;}
-    public double remaining {get;set;}
+    public Status status {get;private set;}
+    public double filled {get;private set;}
+    public double remaining {get;private set;}
 
-    public Order(int id, Side side, double quantity, decimal price)
+    public bool fill_order(double amount)
     {
-       this.order_id = id;
+        filled += amount;
+        remaining = quantity - filled;
+        if (remaining > 0)
+        {
+            status = Status.partially_filled;
+            return false;
+        }
+        status = Status.filled;
+        return true;
+    }
+
+    public Order(Side side, double quantity, decimal price)
+    {
+       this.order_id = Order.order_id_counter++;
        this.order_side = side;
        this.quantity = quantity;
        this.price = price;
